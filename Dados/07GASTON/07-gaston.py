9beec17a262024875b7791b6e1b635ac18a44c76
vitor navarro <vitor.san.nav.nexus@gmail.com>
2020-11-03 12:27:51 -0300

    added stuff to gaston's lab 7

--- Dados/07GASTON/07-gaston.py
+++ Dados/07GASTON/07-gaston.py
@@ -1,3 +1,4 @@
+import math
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -12,16 +13,99 @@ plt.tight_layout(pad=2)
 plt.subplot(2, 3, 2)
 plt.tight_layout(pad=2)
 
-plt.subplot(2, 3, 3)
-plt.tight_layout(pad=2)
-
-plt.subplot(2, 3, 4)
-plt.tight_layout(pad=2)
-
-plt.subplot(2, 3, 5)
-plt.tight_layout(pad=2)
-
-plt.subplot(2, 3, 6)
-plt.tight_layout(pad=2)
-
 plt.show()
+
+maximo = data.index[data["Cars Sold"] == data["Cars Sold"].max()].tolist()
+nomes_max = [
+    "August Looner",
+    "Brad Withers",
+    "Brad Michaels",
+    "Brad Baker",
+    "Carla Mulgrew",
+    "Charles Kennedy",
+    "Charles Turner",
+    "Charles Davidson",
+    "Frank Kane",
+    "John Franklin",
+    "Lisa Michaels",
+    "Onika Turner",
+    "Paula Sears",
+    "Peter Rogers",
+    "Ronelle Turner",
+    "Sam Davidson",
+    "Sam Turner",
+    "Sam Johnson",
+    "Sam Monroe",
+    "Sam Patterson",
+    "Tom Davidson",
+    "Tom Kane",
+    "Tom Davidson",
+    "Victoria Kennedy",
+    "Walter Kane",
+]
+
+minimo = data.index[data["Cars Sold"] == data["Cars Sold"].min()].tolist()
+nomes_min = [
+    "Adam Looner",
+    "Adam Adams",
+    "August Mulgrew",
+    "Betty Jackson",
+    "Betty Sapp",
+    "Betty Kennedy",
+    "Brad Vaughn",
+    "Brad Mulgrew",
+    "Carla Ettienne",
+    "Carla Baker",
+    "Carla Isaacson",
+    "Carla Ettienne",
+    "Carla Looner",
+    "Carla Jackson",
+    "Charles Walters",
+    "Charles Walters",
+    "Denise Samuelson",
+    "Francine Henderson",
+    "Frank Johnson",
+    "Frank Samuelson",
+    "Frank Rogers",
+    "Jack Adams",
+    "Jack Carter",
+    "Jackie Rose",
+    "Jackie Franklin",
+    "Jada Johnson",
+    "Jada Rose",
+    "John Mulgrew",
+    "Karen Samuelson",
+    "Lisa Martins",
+    "Mary Franklin",
+    "Nicole Moore",
+    "Onika Turner",
+    "Paula Isaacson",
+    "Peter Adams",
+    "Peter Davidson",
+    "Roger Kennedy",
+    "Roger Sapp",
+    "Samantha Johnson",
+    "Samantha Sears",
+    "Samantha Rogers",
+    "Tom Looner",
+    "Victoria Rogers",
+    "Walter Mulgrew",
+]
+
+print(f"Média de carros vendidos no ano: {round(data['Cars Sold'].mean())}")
+
+print("------------------------------------------------------------------------")
+print(f"O valor máximo de carros vendidos: {data['Cars Sold'].max()}\n")
+print(
+    f"Os índices dos vendedores são:\n{', '.join(str(i) for i in maximo)}\n\nSeus nomes são:"
+)
+for i in nomes_max:
+    print(i)
+
+print("------------------------------------------------------------------------")
+print(f"O valor mínimo de carros vendidos: {data['Cars Sold'].min()}\n")
+print(
+    f"Os índices dos vendedores são:\n{', '.join(str(i) for i in minimo)}\n\nSeus nomes são:"
+)
+for i in nomes_min:
+    print(i)
